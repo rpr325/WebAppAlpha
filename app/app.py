4f5e619b80cfe7b4f77d97ccf01fff0abec8070d
roruther <Thereisafountain7!>
2020-12-07 13:19:54 -0500

    Feature: app.py file config.

--- app/app.py
+++ app/app.py
@@ -27,15 +27,15 @@ def cities_import() -> List[Dict]:
     return result
 
 
-#@app.route('/')
-#def index():
-    #user = {'username': 'Miguel'}
-    #cities_data = cities_import()
-
-    #return render_template('index.html', title='Home', user=user, cities=cities_data)
-
-
 @app.route('/')
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+@app.route('/api/cities')
 def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
